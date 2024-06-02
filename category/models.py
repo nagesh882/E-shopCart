42c907754790ad8af42a8d1ad89c462528c06c52
Nagesh <yashchavan192001@gmail.com>
2024-06-03 04:06:38 +0530

    category model class created

--- category/models.py
+++ category/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# I create model class as category name
+
+class Category(models.Model):
+    category_id = models.BigAutoField(primary_key=True, unique=True)
+    category_name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    category_image = models.ImageField(upload_to="photos/categories", blank=True)
+
+
+    def __str__(self):
+        return self.category_name
