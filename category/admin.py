7f96f7cf0c6663c6e20d46ad11843e4a0faeb19b
Nagesh <yashchavan192001@gmail.com>
2024-06-04 21:12:03 +0530

    Initialize project

--- category/admin.py
+++ category/admin.py
@@ -4,11 +4,9 @@ from category.models import Category
 
 
 
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"category_slug":("category_name",)}
     ordering = ["-category_id"]
     list_display = ["category_name", "category_slug"]
     list_display_links = ["category_name", "category_slug"]
-
