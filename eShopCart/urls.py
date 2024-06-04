bbc27c3a24cfbf6880c2fe622a9b5f348817c618
Nagesh <yashchavan192001@gmail.com>
2024-06-05 00:53:44 +0530

    products added and I have written their views with urls

--- eShopCart/urls.py
+++ eShopCart/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.homePage, name="homePage"),
     
 ]
 
