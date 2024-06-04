bfba8d30580a052f20819d05b36ba9be4baf2ad6
Nagesh <yashchavan192001@gmail.com>
2024-06-04 23:00:52 +0530

    Store app created and Product model is created

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from store.models import Product
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'product_slug':('product_name',)}
+    ordering = ['-product_id']
+    list_display = ['product_name', 'price', 'stock', 'category', 'modified_date']
+    list_display_links = ['product_name', 'category']
--- store/models.py
+++ store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+# I create model class as Product name
+
+
+class Product(models.Model):
+    product_id = models.BigAutoField(primary_key=True, unique=True)
+    product_name = models.CharField(max_length=100, unique=True)
+    product_slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    product_image = models.ImageField(upload_to="photos/products", blank=True)
+    stock = models.IntegerField()
+    is_availabel = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    
+    def __str__(self):
+        return self.product_name
