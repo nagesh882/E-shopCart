b58542ff46368892831695aaf0fce4de57d1c372
Nagesh <yashchavan192001@gmail.com>
2024-06-20 16:54:49 +0530

    registration form improved and it rendered in register page

--- account/forms.py
+++ account/forms.py
@@ -22,11 +22,22 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number']
-
+        labels = {
+            'phone_number':'Phone',
+            'first_name': 'First Name',
+            'last_name': 'Last Name' 
+        }
 
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.label_suffix = '' # I not use default label suffix
+
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email address'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
--- account/views.py
+++ account/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from account.forms import RegistrationForm
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+
+    if request.method == "POST":
+        register_form = RegistrationForm()
+    else:
+        register_form = RegistrationForm()
+
+    context={
+        'form': register_form,
+    }
+
+    return render(request, 'accounts/register.html', context)
 
 
 def sign_in(request):
